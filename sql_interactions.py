d7364334b5957719d332ef59d53838c488f2be77
sirecactius <81409000+sirecactius@users.noreply.github.com>
2021-03-26 11:11:28 +0100

    Add files via upload

---
+++ sql_interactions.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect('main.db')
+c = conn.cursor()
+
+
+def loginSQL(pseudo, password):
+    c.execute(f'SELECT user_id FROM user WHERE user.username = {pseudo} and user.password = {password}')
+    if c.fetchall() == []:return None
+    L = c.fetchall()
+    conn.close()
+    return L[0]
+
+def signupSQL(pseudo, password):
+    c.execute(f'SELECT user_id FROM user WHERE user.username = {pseudo}')
+    if c.fetchall() != []:
+        return 'None'
+    c.execute(f'INSERT INTO user (username, password) ({pseudo},{password})')
+    conn.commit()
+    conn.close()
+    return f'[new user] {pseudo}: {password}'
