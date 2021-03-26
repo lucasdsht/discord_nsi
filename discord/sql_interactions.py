ab0616a9503b83dcb82431bd51d740ed3201c97f
lucasdsht <72457669+lucasdsht@users.noreply.github.com>
2021-03-26 11:17:14 +0100

    Update sql_interactions.py

--- discord/sql_interactions.py
+++ discord/sql_interactions.py
@@ -12,10 +12,10 @@ def loginSQL(pseudo, password):
     return L[0]
 
 def signupSQL(pseudo, password):
-    c.execute('SELECT user_id FROM user WHERE user.username = pseudo')
+    c.execute(f'SELECT user_id FROM user WHERE user.username = {pseudo}')
     if c.fetchall() != []:
         return 'None'
-    c.execute('INSERT INTO user (username, password) (?, ?)', (pseudo, password))
+    c.execute(f'INSERT INTO user (username, password) ({pseudo},{password})')
     conn.commit()
     conn.close()
     return f'[new user] {pseudo}: {password}'
